32f475ca66a8e61185f8ec516c2592f6b605f339
oalouat <oussama.alouat@accenture.com>
2021-09-13 11:12:55 +0200

    Add loading component

---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div class="ui segment loading-container">
+      <div class="ui active dimmer">
+        <div class="ui text loader">Loading</div>
+      </div>
+      <p></p>
+    </div>
+  )
+}
+
+export default Loading;
