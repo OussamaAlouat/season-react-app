3789cea4c8aa3304496b0bc540b46ccae7a1d4e1
oalouat <oussama.alouat@accenture.com>
2021-09-13 13:44:17 +0200

    Create a default props

--- src/Loading.js
+++ src/Loading.js
@@ -4,11 +4,15 @@ const Loading = (props) => {
   return (
     <div className="ui segment loading-container">
       <div className="ui active dimmer">
-        <div className="ui text loader">{ props.message }</div>
+        <div className="ui text loader">{ props.message || 'Loading ...' }</div>
       </div>
       <p></p>
     </div>
   )
+}
+
+Loading.defaultProps = {
+  message: 'Loading ...'
 }
 
 export default Loading;
